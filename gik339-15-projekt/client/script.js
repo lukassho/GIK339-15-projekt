565ea3d530933ec64c8b36f9dc03a21ebc553832
lukassho <110125953+lukassho@users.noreply.github.com>
2025-01-08 17:04:17 +0100

    Test av färgval

--- gik339-15-projekt/client/script.js
+++ gik339-15-projekt/client/script.js
@@ -6,10 +6,11 @@ function fetchData() {
     fetch(url)
   .then(result => result.json())
   .then(groceries => {
+    if (groceries.length > 0) {
     let html = `<ul class="p-0">`; //LÄGG TILL KLASSER
     groceries.forEach(grocery => {
       html += `<section>
-                <div class="mb-3 p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3 col-8 offset-2 col-xl-3 offset-xl-0">
+                <div class="mb-3 p-3 text-primary-emphasis bg-${grocery.groceryCategory} border border-primary-subtle rounded-3 col-8 offset-2 col-xl-3 offset-xl-0">
                   <li>
                     <h3>${grocery.groceryType}</h3>
                     <p>${grocery.amount} st</p>
@@ -33,5 +34,38 @@ function fetchData() {
     const listContainer = document.getElementById('listContainer');
     listContainer.innerHTML = '';
     listContainer.insertAdjacentHTML('beforeend', html);
+  }
   });
 }
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const serverGroceryObject = {
+    groceryType: '',
+    amount: '',
+    brand: '',
+    groceryCategory: '',
+    note: ''
+  };
+  serverGroceryObject.groceryType = groceryForm.groceryType.value;
+  serverGroceryObject.amount = groceryForm.amount.value;
+  serverGroceryObject.brand = groceryForm.brand.value;
+  serverGroceryObject.groceryCategory = groceryForm.groceryCategory.value;
+  serverGroceryObject.note = groceryForm.note.value;
+
+const request = new Request(url, {
+  method: 'POST',
+  headers: {
+    'content-type': 'application'
+  },
+  body: JSON.stringify(serverGroceryObject)
+});
+
+fetch(request).then((response) => {
+  fetchData();
+  groceryForm.reset();
+})
+
+}
+
+console.log()
